8cae489a9baca3f497c77993031a723a157707ed
3ddelano <3ddelano@gmail.com>
2021-07-18 16:45:48 +0530

    updated icons, added comments

--- project/src/scenes/tile/tile.gd
+++ project/src/scenes/tile/tile.gd
@@ -5,10 +5,12 @@ var number
 signal tile_pressed
 signal slide_completed
 
+# Update the number of the tile
 func set_text(new_number):
 	number = new_number
 	$Number/Label.text = str(number)
 
+# Update the background image of the tile
 func set_sprite(new_frame, size, tile_size):
 	var sprite = $Sprite
 
@@ -18,8 +20,8 @@ func set_sprite(new_frame, size, tile_size):
 	sprite.set_vframes(size)
 	sprite.set_frame(new_frame)
 
+# scale to the new tile_size
 func update_size(size, tile_size):
-	# scale to the tile_size
 	var new_size = Vector2(tile_size, tile_size)
 	set_size(new_size)
 	$Number.set_size(new_size)
@@ -30,19 +32,24 @@ func update_size(size, tile_size):
 	var to_scale = size * (new_size / $Sprite.texture.get_size())
 	$Sprite.set_scale(to_scale)
 
+# Update the entire background image
 func set_sprite_texture(texture):
 	$Sprite.set_texture(texture)
 
+# Slide the tile to a new position
 func slide_to(new_position, duration):
 	var tween = $Tween
 	tween.interpolate_property(self, "rect_position", null, new_position, duration, Tween.TRANS_QUART, Tween.EASE_OUT)
 	tween.start()
 
+# Hide / Show the number of the tile
 func set_number_visible(state):
 	$Number.visible = state
 
+# Tile is pressed
 func _on_Tile_pressed():
 	emit_signal("tile_pressed", number)
 
+# Tile has finished sliding
 func _on_Tween_tween_completed(_object, _key):
 	emit_signal("slide_completed", number)
